09a1896d50be397c0c080130d0bad9ea547eeeda
Brian Yang <yangbri1@msu.edu>
2025-01-17 20:00:52 +0000

    Implemented String manipulaton techniques, String concatenation, alongside for loop & unary operator  (+=) to get task done.

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,26 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        // char[] strToCharAry = in.toCharArray();
+        // char[] charAry = java.util.Arrays.copyOfRange(strToCharAry, 1, strToCharAry.length);
+
+        String emptyStr = "";
+
+        /* this converting to from String -> char[] & indexing through char[] array works as well */
+        // for(int i = 1; i < strToCharAry.length; ++i){
+        //     emptyStr += strToCharAry[i];
+        // }
+
+        // loop through String directly
+        for(int i = 1; i < in.length(); ++i){
+            // start appending at index 1 ...
+            emptyStr += in.charAt(i);
+        }
+
+        // use unary operator '+=' to concatenate the pig latin after String manipulation above
+        emptyStr += in.charAt(0) + "ay";
+        
+        // return completed pig latin phrase
+        return emptyStr;
     }
 }
